e1f78caa6454f60c1481894ebb779eb3c5159bf4
Bronwyn <bronellerbeck@gmail.com>
2023-09-12 19:16:33 +0200

    init contract

--- contracts/GoatContract.sol
+++ contracts/GoatContract.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract GoatContract is IERC721 {
+contract GoatContract is ERC721 {
+
+    struct Goat {
+        uint goatId;
+    }
+
+    constructor() ERC721("Goats", "GOAT") {
+        
+    }
 
 }
